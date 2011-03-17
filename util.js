ddacb18f3c64e3f8d28a95213f5465b987b1718f
jeresig <jeresig@gmail.com>
2011-03-16 22:57:59 -0400

    Initial import from jeresig/trie-js.
    Fix rogue endline that snuck through the build process.
    
    Use Benchmark.js for performance testing.
    
    Rewrite tests to use Benchmark.js. Add the while(true){} loops back in to make it easier to analyze the memory usage of the code.
    
    Mention the blog post on JavaScript Trie Performance Analysis in the README.
    
    Update to Benchmark.js 0.1.348.
    
    Add in tests for succinct trie and binary search string.
    
    Add a simple test for checking the lookup time of a Succinctly-stored Trie.
    
    Change the ORD() function to an object for faster lookups, based upon a suggestion from Steve Hanov.
    
    Fix finding repeated word bug
    
    Finding 'aalsaals' returned true if 'aals' is in trie.

--- util.js
+++ util.js
@@ -1,13 +1,24 @@
-var dict;
+var dict,
+	FrozenTrie = require("./Bits.js").FrozenTrie;
 
 exports.buildTrie = function( txt ) {
 	return (dict = eval( "(" + txt + ")" ));
 };
 
+exports.buildBinaryDict = function( txt ) {
+	return (dict = txt.split(","));
+};
+
+exports.buildSuccinctDict = function( txt ) {
+	var parts = txt.split(",");
+	
+	return (dict = new FrozenTrie( parts[2], parts[1], parts[0] ));
+};
+
 exports.buildStringDict = function( txt ) {
 	return (dict = " " + txt + " ");
 };
-	
+
 exports.buildHashDict = function( txt ) {
 	var words = txt.split(" "),
 		tmp = {};
@@ -15,13 +26,17 @@ exports.buildHashDict = function( txt ) {
 	for ( var i = 0, l = words.length; i < l; i++ ) {
 		tmp[ words[i] ] = true;
 	}
-	
+
 	return (dict = tmp);
 };
 
 exports.findTrieWord = function findTrieWord( word, cur ) {
+	if ( cur === 0 ) {
+		return false;
+	}
+
 	cur = cur || dict;
-	
+
 	for ( var node in cur ) {
 		if ( word.indexOf( node ) === 0 ) {
 			var val = typeof cur[ node ] === "number" && cur[ node ] ?
@@ -36,8 +51,42 @@ exports.findTrieWord = function findTrieWord( word, cur ) {
 			}
 		}
 	}
+
+	return false;
+};
+
+exports.findBinaryWord = function( word ) {
+	var l = word.length;
+	
+	if ( !dict[l] ) {
+		return false;
+	}
+	
+	var words = dict[l].length / l,
+		low = 0, high = words - 1, mid = Math.floor( words / 2 );
+		
+	while ( high >= low ) {
+		var found = dict[l].substr( l * mid, l );
+		
+		if ( word === found ) {
+			return true;
+		}
+		
+		if ( word < found ) {
+			high = mid - 1;
+		
+		} else {
+			low = mid + 1;
+		}
+		
+		mid = Math.floor( (low + high) / 2 );
+	}
 	
 	return false;
+};
+
+exports.findSuccinctWord = function( word ) {
+	return dict.lookup( word );
 };
 
 exports.findStringWord = function( word ) {
