461b009d8641241d00ebea460a05cceeb583b361
Josh Oldenburg <joshthegeek@me.com>
2014-06-15 21:13:03 -0400

    Add index.js.

---
+++ index.js
@@ -0,0 +1,16 @@
+var find = exports.find = function(word, trie, cur) {
+	cur = cur || trie;
+
+	for (var node in cur) {
+		if (word.indexOf(node) === 0) {
+			var val = (typeof cur[node] === "number" && cur[node] ? trie.$[cur[node]] : cur[node]);
+
+			if (node.length === word.length) return val === 0 || val.$ === 0;
+			else return find(word.slice(node.length), trie, val);
+		}
+	}
+
+	return false;
+}
+
+console.log(find('googleearth', eval('(' + require('fs').readFileSync('dict/suffix.js', 'utf8') + ')')));
