bc16af698c125299b63ea5ab306c2eeb819d0d4b
Josh Oldenburg <joshthegeek@me.com>
2014-06-16 09:29:47 -0400

    Add .create(words).

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
+var create = exports.create = function(words) {
+	var trie = {};
+
+	for (var i = 0, wordsLength = words.length; i < wordsLength; i++) {
+		var word = words[i];
+		var letters = word.split('');
+		var cur = trie;
+
+		for (var j = 0; j < letters.length; j++) {
+			var letter = letters[j];
+			var pos = cur[letter];
+
+			if (pos == null) cur = cur[letter] = (j === letters.length - 1 ? 0 : {});
+			else if (pos === 0) cur = cur[letter] = { $: 0 };
+			else cur = cur[letter];
+		}
+	}
+
+	_optimize(trie);
+	_optimizeSuffixes(trie, end);
+
+	for (var key in end) {
+		if (end[key].count <= 10) continue;
+		keepEnd[key] = endings.length;
+		endings.push(end[key].obj);
+	}
+
+	_optimizeFinishSuffixes(trie, keepEnd, end);
+
+	trie.$ = endings;
+
+	return _stringifyTrie(trie);
+}
+
+/*
+ * Search for a word in a trie.
+ *
+ * @param word String The word to look for in the trie
+ * @param trie Object The trie object (@see load)
+ * @param cur null Do not pass this
+ *
+ * @return Boolean Whether the word is in the trie
+ */
 var find = exports.find = function(word, trie, cur) {
 	cur = cur || trie;
 
@@ -11,4 +54,69 @@ var find = exports.find = function(word, trie, cur) {
 	}
 
 	return false;
+}
+
+// Private functions
+function _optimize(cur) {
+	var num = 0;
+	var last;
+
+	for (var node in cur) {
+		if (typeof cur[node] === 'object') {
+			var ret = _optimize(cur[node]);
+
+			if (ret) {
+				delete cur[node];
+				cur[node + ret.name] = ret.value;
+				node = node + ret.name;
+			}
+		}
+
+		last = node;
+		num++;
+	}
+
+	if (num === 1) return { name: last, value: cur[last] };
+}
+
+function _optimizeSuffixes(cur, end) {
+	var hasObject = false;
+	var key = '';
+
+	for (var node in cur) {
+		if (typeof cur[node] === 'object') {
+			hasObject = true;
+
+			var ret = _optimizeSuffixes(cur[node], end);
+			if (ret) cur[node] = ret;
+		}
+
+		key += ',' + node;
+	}
+
+	if (!hasObject) {
+		if (end[key]) end[key].count++;
+		else end[key] = { obj: cur, count: 1 };
+
+		return key;
+	}
+}
+
+function _optimizeFinishSuffixes(cur, keepEnd, end) {
+	for (var node in cur) {
+		var val = cur[node];
+
+		if (typeof val === 'object') _optimizeFinishSuffixes(val, keepEnd, end);
+		else if (typeof val === 'string') cur[node] = keepEnd[val] || end[val].obj;
+	}
+}
+
+function _stringifyTrie(trie) {
+	var ret = JSON.stringify(trie).replace(/"/g, '');
+
+	for (var i = 0; i < reserved.length; i++) {
+		ret = ret.replace(new RegExp('([{,])(' + reserved[i] + '):', 'g'), "$1'$2':");
+	}
+
+	return ret;
 }
